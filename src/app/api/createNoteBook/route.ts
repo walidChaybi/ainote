e3655a2ef8adf43bc4c84a6eb6d3b70bbd0b1f7a
walidChaybi <98161047+walidChaybi@users.noreply.github.com>
2023-11-08 16:31:08 +0100

    added delete funcionality

--- src/app/api/createNoteBook/route.ts
+++ src/app/api/createNoteBook/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: Request) {
   });
 
   try {
-    const imageUrl:= await replicate.run(
+    const imageUrl = await replicate.run(
       "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
       {
         input: {
